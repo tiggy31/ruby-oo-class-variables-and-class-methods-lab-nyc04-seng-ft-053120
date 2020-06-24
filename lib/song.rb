717e7d6934d094b52c085de34aa432681a44943c
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-24 11:48:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
  
-  attr_reader :name, :artist, :genre
-  
+  attr_reader :name
+  attr_accessor :name,:artist,:genre
   def initialize(name,artist, genre)
    @@count = 3
   @@genres =["rap", "rap", "pop"]
