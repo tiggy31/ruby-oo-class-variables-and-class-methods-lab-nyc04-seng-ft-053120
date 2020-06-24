cfe57bd712dc354f2ebab1a73a7c2fe969ce75ee
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-24 11:52:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   
   attr_accessor :name,:artist,:genre
   def initialize(name,artist, genre)
-   @@count +=1 
+   @@count = 0
   @@genres =["rap", "rap", "pop"]
   @@artists=["Jay-Z", "Brittany Spears"].uniq
   @@genre_count= {}
@@ -13,7 +13,10 @@ class Song
   @genre = genre
 end
 
-
+def count
+  @@count +=1 
+  
+end
 
   
 end
